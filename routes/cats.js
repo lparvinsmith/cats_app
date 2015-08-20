25128d8157959ddedf029d88078ca2f747f3ccd6
Lara Parvinsmith <lara.parvinsmith@gmail.com>
2015-08-20 14:50:23 -0400

    adds route for cat/:id PATCH, which updates cat

--- routes/cats.js
+++ routes/cats.js
@@ -21,14 +21,35 @@ router.route('/')
   });
 
 router.route('/:id')
+  .all(function(req,res,next){
+    //add cat id as property of response: makes res.locals.cat available to each of following handlers
+    models.Cat.findById(req.params.id).then(function(cat){
+      res.locals.cat = cat;
+      next();
+    }, function(err){
+      next(err); //goes to fail handler in app, unless we set fail handler below with all
+    });
+  })
   .get(function(req,res){
     res.send("cats#show");
   })
   .patch(function(req,res){
-    res.send("cats#update");
+    // res.locals.cat stores cat, .update updates db using req.body
+    res.locals.cat.update(req.body).then(function(cat){
+      res.json(cat);
+    }, function(err){
+      res.sendStatus(500);
+    });
   })
   .delete(function(req,res){
     res.send("cats#delete");
   })
+  // fail handler -- next(err) leads here
+  .all(function(err, req, res, next){
+    if (err){
+      res.sendStatus(404);
+    }
+  })
+
 
 module.exports = router;
